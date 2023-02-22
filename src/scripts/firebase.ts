61c0f723697d736cd85fc3133bfacd8b15f2cf74
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:21 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDY8gxW4POoBJbhYJl9s6ZSZ3WlVcKTCww",
-	appId: "1:890963614794:web:663adf5b59dad366d1dab3",
 	authDomain: "returnthief.firebaseapp.com",
-	measurementId: "G-D1GFQDDFPD",
-	messagingSenderId: "890963614794",
 	projectId: "returnthief",
 	storageBucket: "returnthief.appspot.com",
+	messagingSenderId: "890963614794",
+	appId: "1:890963614794:web:663adf5b59dad366d1dab3",
+	measurementId: "G-D1GFQDDFPD",
 };
 
 initializeApp(firebaseConfig);
