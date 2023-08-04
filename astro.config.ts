046bf020e6aec0a6b3906b0d73770a52976d382b
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-04 20:12:35 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import worker from "astrojs-service-worker";
 export default defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
+	outDir: "./Build",
 	site: "https://returnthief.quest",
 	experimental: {
 		assets: true,
