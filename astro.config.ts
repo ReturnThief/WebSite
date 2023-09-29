b66b649b7d7797ed8123fef08d9a270d697f8b1e
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-29 14:14:35 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> upstream/main
 export default (await import("astro/config")).defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
@@ -9,23 +5,14 @@ export default (await import("astro/config")).defineConfig({
 	site: "https://returnthief.quest",
 	compressHTML: true,
 	integrations: [
-<<<<<<< HEAD
-		import.meta.env.MODE === "production" ? (await import("astrojs-service-worker")).default() : null,
-		(await import("@astrojs/sitemap")).default(),
-		(await import('astro-critters')).default({ Logger: 1 }),
-		(await import('@astrojs/prefetch')).default(),
-		(await import('astro-rome')).default({ Logger: 1 }),
-		(await import('astro-compress')).default({ Logger: 1 }),
-=======
 		import.meta.env.MODE === "production"
-		? (await import("astrojs-service-worker")).default()
-		: null,
+			? (await import("astrojs-service-worker")).default()
+			: null,
 		(await import("@astrojs/sitemap")).default(),
 		(await import("astro-critters")).default({ Logger: 1 }),
 		(await import("@astrojs/prefetch")).default(),
 		(await import("astro-rome")).default({ Logger: 1 }),
 		(await import("astro-compress")).default({ Logger: 1 }),
->>>>>>> upstream/main
 	],
 	vite: {
 		build: {
