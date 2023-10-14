95d638f46c621e29cce653eb3c50b77636621089
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 13:44:18 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,6 +1,4 @@
-import { initializeApp } from "firebase/app";
-
-initializeApp({
+export default (await import("firebase/app")).initializeApp({
 	apiKey: "AIzaSyDY8gxW4POoBJbhYJl9s6ZSZ3WlVcKTCww",
 	authDomain: "returnthief.firebaseapp.com",
 	projectId: "returnthief",
